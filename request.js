c0151348df7e4e1b3e5946113889c8661e83a15b
Garret Gallant <gallanttechnological@gmail.com>
2023-02-13 21:24:13 -0700

    Compiled js file from TS to reflect accurate changes

--- request.js
+++ request.js
@@ -3,5 +3,12 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = "https://jsonplaceholder.typicode.com/users/1";
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data.name);
+    var user = response.data;
+    var id = user.id;
+    var name = user.name;
+    var email = user.email;
+    logUser(id, name, email);
 });
+var logUser = function (id, name, email) {
+    console.log("\n  The User id is: ".concat(id, "\n  Their name is ").concat(name, "\n  Their email is: ").concat(email, "\n  "));
+};
